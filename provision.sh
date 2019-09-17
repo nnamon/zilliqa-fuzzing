8ca7c0be360a2ea0955d1883b7de3cfeee346b45
amon <amon@nandynarwhals.org>
2019-09-17 23:29:01 +0800

    Add provisioning script.

---
+++ provision.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -ex
+
+# Swap - Zilliqa is resource hungry.
+
+SIZE=8G
+
+fallocate -l $SIZE /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+cp /etc/fstab /etc/fstab.bak
+echo '/swapfile none swap sw 0 0' | tee -a /etc/fstab
+
+# Docker
+
+apt update
+apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+apt update
+apt install -y docker-ce
