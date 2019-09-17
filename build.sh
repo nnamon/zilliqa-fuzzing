9d9b63edebbab343b25fe698ad57668489d271f2
amon <amon@nandynarwhals.org>
2019-09-18 01:44:14 +0800

    .

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ IMAGE=zilliqa-docker
 if [ "$#" -lt 1 ]; then
     docker build -t $IMAGE .
 else
-    docker build --build-arg "ZILLIQA_VERSION=$1" --build-arg "FORCE_UPDATE=no$2" -t "$IMAGE:$1" .
+    docker build --build-arg "ZILLIQA_VERSION=$1" --build-arg "FORCE_REBUILD=no$2" -t "$IMAGE:$1" .
 fi
 
